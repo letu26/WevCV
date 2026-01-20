bcd2868a62bf92e2d8ed7ff10d4d2477be76bc4b
Tu <tu260203@gmail.com>
2026-01-20 15:05:41 +0700

    update

---
+++ webcv_frontend/src/layout/user/UserLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserLayout: React.FC = () => {
+  return (
+    <>
+      <div>UserLayout</div>
+    </>
+  );
+};
+
+export default UserLayout;
---
+++ webcv_frontend/src/types/UserType.ts
@@ -0,0 +1,7 @@
+export interface User{
+  id:number;
+  fullname: string;
+  username: string;
+  password: string;
+  email: string;
+}
