6e27377854d9639d1ecf94ad107a50641ad8488e
Tu <tu260203@gmail.com>
2026-01-20 17:23:09 +0700

    update ver2

--- webcv_frontend/src/pages/SignInPage.tsx
+++ webcv_frontend/src/pages/SignInPage.tsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import { fetcher } from '@/api/Fetcher';
+import type { User } from '@/types/UserType';
+import React, { useState, useEffect } from 'react';
 
 const SignInPage: React.FC = () => {
+
+  //ví dụ khi muốn lấy danh sách user:
+  const [user, setUser] = useState<User[]>([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const data = await fetcher<User[]>({
+          url: "/users",
+          method: "get",
+        });
+        setUser(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+  console.log(user);
   return (
     <>
       <div>SignInPage</div>
