c97e47ad1b8514f81085c63e8136f6aaaa2ac392
Tu <tu260203@gmail.com>
2026-01-27 16:34:00 +0700

    b

--- webcv_frontend/vite.config.ts
+++ webcv_frontend/vite.config.ts
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
-import taiwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), taiwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
-
