5a8c11a2c8d3de92a54e2a8716fb97f45538fd84
pmanh24 <manhphung2405@gmail.com>
2026-02-03 18:20:44 +0700

    up admin manage project

--- webcv_backend/src/main/java/com/webcv/response/admin/MemberResponse.java
+++ webcv_backend/src/main/java/com/webcv/response/admin/MemberResponse.java
@@ -3,6 +3,24 @@ package com.webcv.response.admin;
 public class MemberResponse {
     private String fullname;
     private String email;
+    private Long id;
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 
     public String getFullname() {
         return fullname;
