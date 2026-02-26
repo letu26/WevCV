f255bc8d40069de54e97e600ae0935277e0f3316
pmanh24 <manhphung2405@gmail.com>
2026-02-26 18:19:17 +0700

    fix cors

--- webcv_backend/src/main/java/com/webcv/config/WebSecurityConfig.java
+++ webcv_backend/src/main/java/com/webcv/config/WebSecurityConfig.java
@@ -56,22 +56,5 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of(
-                "https://fe-cv-alpha.vercel.app"
-        ));
-
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return source;
-    }
 }
