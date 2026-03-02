5dc84f9e453190382582c20547149bfc4825fce1
Tu <tu260203@gmail.com>
2026-03-02 08:30:18 +0700

    change allowedOrigins

--- webcv_backend/src/main/java/com/webcv/config/SecurityConfig.java
+++ webcv_backend/src/main/java/com/webcv/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-                "https://wev-cv.vercel.app",
+                "https://wev-cv-git-deploy-tus-projects-e61aca52.vercel.app",
                 "http://localhost:5173"
         ));
 
